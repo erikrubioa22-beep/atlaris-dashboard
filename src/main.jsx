da1d9dd4247462bdbcf7c9265346c74ac6b84f05
Atlaris Technologies <erikrubioa22@gmail.com>
2026-08-15 15:58:12 +0000

    Improve Atlaris operations dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import FinanceBridge from './FinanceBridge.jsx'
+import FinanceOperations from './FinanceOperations.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <FinanceBridge />
+    <FinanceOperations />
   </StrictMode>,
 )
